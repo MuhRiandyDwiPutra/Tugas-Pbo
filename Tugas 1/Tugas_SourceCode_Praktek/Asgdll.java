19458db1fe7c9c705ddd26f4d51b70093a7305b8
MuhRiandyDwiPutra <161152986+MuhRiandyDwiPutra@users.noreply.github.com>
2024-02-25 20:49:08 +0800

    Add files via upload

---
+++ Tugas 1/Tugas_SourceCode_Praktek/Asgdll.java
@@ -0,0 +1,23 @@
+/*
+Nama:Muh.Riandy Dwi Putra
+Nim :13020220121
+(Minggu/25-2-2024/10:32)
+*/
+
+
+public class Asgdll {
+
+/**
+	* @param args
+	*/
+public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	/* Kamus */
+	float f= 20.0f;
+	double fll;
+
+	/* Algoritma */
+	fll=10.0f;
+	System.out.println ("f : "+f+ "\nf11: "+fll);
+	}
+}
---
+++ Tugas 1/Tugas_SourceCode_Praktek/Incr.java
@@ -0,0 +1,20 @@
+/*
+Nama:Muh.Riandy Dwi Putra
+Nim :13020220121
+(Minggu/25-2-2024/12:20)
+*/
+
+
+
+/* Effek dari operator ++ */
+ public class Incr {
+ 	public static void main(String[] args) {
+ 	// TODO Auto-generated method stub
+ 	/* Kamus */
+ 	int i, j;
+ 	/* Program */
+ 	i = 3;
+ 	j = i++;
+ 	System.out.println ("Nilai i : " + (++i) + "\nNilai j : " + j);
+ 	}
+ }
