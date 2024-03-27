365848afb74fa3dd702f209a06a142a9489c62f7
MuhRiandyDwiPutra <161152986+MuhRiandyDwiPutra@users.noreply.github.com>
2024-03-27 22:16:56 +0800

    Add files via upload

---
+++ Tugas 4/Tugas4_SourceCode/HitungNilaiAkhir.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Evaluasi.HitungNilai;
+
+/**
+ *
+ * @author ASUS TUF GAMING
+ */
+public class HitungNilaiAkhir {
+    public class NilaiMahasiswa {
+    private int tugas1;
+    private int tugas2;
+    private int mid;
+    private int uas;
+
+    // Method untuk menghitung nilai rata-rata tugas
+    public double nilaiTugas(int tugas1, int tugas2) {
+        return (tugas1 + tugas2) / 2.0; // Menggunakan 2.0 untuk memastikan hasilnya bertipe double
+    }
+
+    // Method untuk menghitung nilai akhir
+    public double nilaiAkhir(double tugas, int mid, int uas) {
+        return (tugas * 0.4) + (mid * 0.3) + (uas * 0.3);
+    }
+
+    // Getter dan setter lainnya...
+}
+
+}
---
+++ Tugas 4/Tugas4_SourceCode/Orang.java
@@ -0,0 +1,22 @@
+/*Nama : Muh. Riandy Dwi Putra
+Nim : 13020220121
+Tanggal : Rabu,27 Maret 2024
+ */
+
+//kode program evaluasi praktikum 4 NO.2
+
+public class Orang {
+    public String nama;
+
+    public Orang() {
+        this.nama = "Aminah";
+    }
+
+    public Orang(String nama) {
+        this.nama = nama;
+    }
+
+    public static void info() {
+        System.out.println("Ini adalah kelas Orang" );
+    }
+}
